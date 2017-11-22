18be1c7c07800944b095645542d2260046a2b86c
iwata_katsuhiro <katsuwo.com@gmail.com>
2017-11-22 23:59:47 +0900

    "hello"を出力するサービスを追加

--- src/main/java/com/itcjapan/line/LineApiApplication.java
+++ src/main/java/com/itcjapan/line/LineApiApplication.java
@@ -2,11 +2,18 @@ package com.itcjapan.line;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class LineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LineApiApplication.class, args);
+	}
+
+	@RequestMapping(value="/")
+	String hello() {
+		return "hello";
+
 	}
 }
